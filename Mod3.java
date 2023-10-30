296337fcd80c59884d9674d4ae14065b3181b9a8
Skysight2023 <124235894+Skysight2023@users.noreply.github.com>
2023-10-30 10:23:09 -0500

    Rock Paper Scissors
    Rock Paper Scissors Program (Completed)

--- Mod3.java
+++ Mod3.java
@@ -1,14 +1,29 @@
 //Alejandro Quezada
 //10/30/2023
-//Module 3 Programming Assignment
+//Module 3 Programming Assignment - Rock-Paper-Scissors
+import java.util.Random;
 import java.util.Scanner;
+//Note for myself: doing "import java.util.*;" will work the same as the imports above
 public class Mod3 {
     public static void main(String[] args) {
         Scanner gameSelect = new Scanner(System.in);
 
         System.out.println("Please select a number: [1:Rock] [2:Paper] [3:Scissors] ");
-        double userChoice = gameSelect.nextDouble();
-        
+        int userChoice = gameSelect.nextInt();
 
+        Random rand = new Random();
+        int computerChoice = rand.nextInt(1,4);
+
+        System.out.println("The computer choose: " + computerChoice + "." + " The user choose: " + userChoice + ".");
+
+        if(userChoice == computerChoice){
+            System.out.println("It was a tie!!!");
+        }
+        else if((userChoice == 1 && computerChoice == 2) || (userChoice == 2 && computerChoice == 3) || (userChoice == 3 && computerChoice == 1)){
+            System.out.println("You lost. :'(");
+        }
+        else if((userChoice == 2 && computerChoice == 1) || (userChoice == 3 && computerChoice == 2) || (userChoice == 1 && computerChoice == 3)){
+            System.out.println("You won! :)");
+        }
     }
 }

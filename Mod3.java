66317e3c8d7c2d34e7ac205c4c20cb06af1ad08f
Skysight2023 <124235894+Skysight2023@users.noreply.github.com>
2023-10-30 09:28:10 -0500

    Create Mod3.java

---
+++ Mod3.java
@@ -0,0 +1,14 @@
+//Alejandro Quezada
+//10/30/2023
+//Module 3 Programming Assignment
+import java.util.Scanner;
+public class Mod3 {
+    public static void main(String[] args) {
+        Scanner gameSelect = new Scanner(System.in);
+
+        System.out.println("Please select a number: [1:Rock] [2:Paper] [3:Scissors] ");
+        double userChoice = gameSelect.nextDouble();
+        
+
+    }
+}

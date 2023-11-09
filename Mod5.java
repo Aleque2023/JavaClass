fc3ac8a09c59893ccf13e0b9553e7a1a2d5846cc
Skysight2023 <124235894+Skysight2023@users.noreply.github.com>
2023-11-09 12:58:17 -0600

    Updated it

--- Mod5.java
+++ Mod5.java
@@ -6,12 +6,33 @@
 public class Mod5{
     public static void main(String [] args){
 
-        double total = 0;
-        double counter = 3;
+        double total1 = 0;
+        double counter1 = 3;
+        double total2 = 0;
+        double counter2 = 99;
 
-        while(counter < 100){
-            total += 1/counter;
+        while(counter1 < 100){
+            double output1 = 1/counter1;
+            total1 = total1 + output1;
+            System.out.println(output1);
+            ++counter1;
         }
-        System.out.println(total);
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+        while(counter2 > 2){
+            double output2 = 1/counter2;
+            total2 = total2 + output2;
+            System.out.println(output2);
+            --counter2;
+        }
+        System.out.println("");
+        System.out.println("The total for smaller to larger order is: " + total1);
+        System.out.println("");
+        System.out.println("The total for larger to smaller order is: " + total2);
+
+
     }
 }

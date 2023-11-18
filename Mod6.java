06b971f84f205882c49273d7e322790842ee24b0
Skysight2023 <124235894+Skysight2023@users.noreply.github.com>
2023-11-17 20:14:04 -0600

    WIP

--- Mod6.java
+++ Mod6.java
@@ -6,10 +6,9 @@ import java.lang.Math;
 
 public class Mod6 {
     public static void main(String[] args) {
-        int d = 0;
+
         int x = 1;
         int y = 0;
-        int counter2 = 64;
 
         for(int a = 0; a < 7; ++a){
 
@@ -22,12 +21,16 @@ public class Mod6 {
                 System.out.print(x + " ");
 
                 for(int counter1 = 1; counter1 <= a;++counter1){
-                int counter = (int) Math.pow(2,counter1);
-                System.out.print(counter + " ");
+                    int counter = (int) Math.pow(2,counter1);
+                    System.out.print(counter + " "); 
                 }
-                
-                for(int z = 6; z >= 2;--z){
-                    System.out.print(" " + (counter2 /(int) Math.pow(2,z)));
+                int f = 1;
+                int e = 64;
+                int counter3 = 7;
+
+                for(int counter2 = 1; counter2 <= a; ++counter2){
+                    --counter3;
+                    System.out.print(counter3 + " ");
                 }
                 
                 

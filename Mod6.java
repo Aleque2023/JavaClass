483b6347217d50f0b561f734d19b08fef4484cb1
Skysight2023 <124235894+Skysight2023@users.noreply.github.com>
2023-11-18 20:28:37 -0600

    Numbers Output
    Numbers are outputting correctly.

--- Mod6.java
+++ Mod6.java
@@ -7,39 +7,39 @@ import java.lang.Math;
 public class Mod6 {
     public static void main(String[] args) {
 
-        int x = 1;
+        int numOne = 1;
         int y = 0;
 
-        for(int a = 0; a < 7; ++a){
 
-            for(int b1 = 7 ; b1 > a ; --b1){
+        for(int start = 0; start < 7; ++start){
+            int powerTwo = start;
+            for(int spaceOne = 15 ; spaceOne > start ; --spaceOne){
                 System.out.print(" ");
             }
 
 
-            for(;y <= a; ++y){
-                System.out.print(x + " ");
+            for(;y <= start; ++y){
+                System.out.print(numOne + " ");
 
-                for(int counter1 = 1; counter1 <= a;++counter1){
-                    int counter = (int) Math.pow(2,counter1);
-                    System.out.print(counter + " "); 
+                for(int powerOne = 1; powerOne <= start;++powerOne){
+                    int ascend = (int) Math.pow(2,powerOne);
+                    System.out.print(ascend + " "); 
                 }
-                int f = 1;
-                int e = 64;
-                int counter3 = 7;
 
-                for(int counter2 = 1; counter2 <= a; ++counter2){
-                    --counter3;
-                    System.out.print(counter3 + " ");
-                }
-                
-                
+
+                for(int k = 1;k <= start; ++k){
+                    int descend = (int) Math.pow(2, powerTwo) / 2;
+                    --powerTwo;
+                    System.out.print(descend + " ");
+                    }
+
             }
+        
 
-
-            for(int b = 7 ; b > a ; --b){
+            for(int spaceTwo = 10 ; spaceTwo > start ;--spaceTwo){
                 System.out.print(" ");
             }
+
 
         System.out.println("@");
 

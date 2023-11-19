3b663b4091cd5ef1ac378c740cef103c89b66ec4
Skysight2023 <124235894+Skysight2023@users.noreply.github.com>
2023-11-19 13:08:47 -0600

    Additional Changes
    Changed formatting and date.

--- Mod6.java
+++ Mod6.java
@@ -1,11 +1,12 @@
 //Alejandro Quezada
-//11/16/2023
+//11/19/2023
 //Module 6 Programming Assignment
 
 import java.lang.Math;
 
 public class Mod6 {
     public static void main(String[] args) {
+
 
         int numOne = 1;
         int loopOne = 0;
@@ -14,6 +15,7 @@ public class Mod6 {
         for(int start = 0; start < 7; ++start){
             int powerTwo = start;
 
+
             for(int spaceOne = 15 ; spaceOne > start ; --spaceOne){
                 System.out.print(" ");
             }
@@ -21,6 +23,7 @@ public class Mod6 {
 
             for(;loopOne <= start; ++loopOne){
                 System.out.print(numOne + " ");
+
 
                 for(int powerOne = 1; powerOne <= start;++powerOne){
                     int ascend = (int) Math.pow(2,powerOne);
@@ -32,20 +35,20 @@ public class Mod6 {
                     int descend = (int) Math.pow(2, powerTwo) / 2;
                     --powerTwo;
                     System.out.print(descend + " ");
-                    }
-
+                }
             }
         
 
             for(int spaceTwo = 9 ; spaceTwo >= start ;--spaceTwo){
                 for(int loopThree = 6; loopThree >= 0;){
-                System.out.print(" ");
-                loopThree -= 3;
+                    System.out.print(" ");
+                    loopThree -= 3;
                 }
             }
 
             
         System.out.println("@");
+
 
         }
     }

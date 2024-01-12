4e67e05ac0ed56cf588f4b263bae78e2b417f148
Alejandro <alejandroquezada020@gmail.com>
2024-01-11 17:45:38 -0800

    update

--- Semester2-(2024)/Module1.java
+++ Semester2-(2024)/Module1.java
@@ -3,7 +3,10 @@
 //Module 1 Programming Assignment
 
 public class Module1 {
+    double ex = 20;
     public static void main(String [] aStrings){
+        Module1 myObj = new Module1();
+        System.out.println(myObj.ex);
         System.out.println("test");
     }
 }

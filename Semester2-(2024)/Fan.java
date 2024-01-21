646ae45e8f648ff80b48306a495e2608653a7231
Skysight2023 <124235894+Skysight2023@users.noreply.github.com>
2024-01-20 19:47:42 -0600

    wip

---
+++ Semester2-(2024)/Fan.java
@@ -0,0 +1,65 @@
+//Alejandro Quezada
+//1/11/2024
+//Module 1 Programming Assignment
+
+public class Fan {
+    static final int STOPPED = 0;
+    static final int SLOW = 1;
+    static final int MEDIUM = 2;
+    static final int FAST = 3;
+
+    private int speed = STOPPED;
+    private boolean on = false;
+    private int radius = 6;
+    private String color = "white";
+
+    public Fan() {
+        this.speed = STOPPED;
+        this.on = false;
+        this.radius = 6;
+        this.color = "white";
+    }
+
+    public Fan(int speed, boolean on, int radius, String color) {
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int newSpeed) {
+        this.speed = newSpeed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean newOn){
+        this.on = newOn;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int newRadius) {
+        this.radius = newRadius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String newColor) {
+        this.color = newColor;
+    }
+
+    public String toString() {
+        return "Fan { " + "speed=" + speed + ", on=" + on + ", radius=" + radius + ", color=" + color + "\"" + " }";
+    }
+}

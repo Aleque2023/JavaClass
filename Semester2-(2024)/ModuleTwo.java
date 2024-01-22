1358a493d8d710f3913515655868c43b2fa28175
Skysight2023 <124235894+Skysight2023@users.noreply.github.com>
2024-01-21 18:45:50 -0600

    Update ModuleTwo.java

--- Semester2-(2024)/ModuleTwo.java
+++ Semester2-(2024)/ModuleTwo.java
@@ -8,20 +8,11 @@ class UseFans {
     Fan inst3 = new Fan(Fan.STOPPED, false, 8, "red");
     Fan inst4 = new Fan(Fan.SLOW, true, 5, "purple");
 
-    public static void displayFans(Fan fan){
-        System.out.println(fan.getSpeed() + ", " + fan.isOn() + ", " + fan.getRadius() + ", " + fan.getColor());
+    public static void displayFans(Fan fan) {
+        System.out.println("Speed: " + fan.getSpeed() + ", Fan: " + fan.isOn() + ", Radius: " + fan.getRadius() + ", Color: " + fan.getColor());
     }
 
-    public static void main(String [] args){
-        UseFans useFans = new UseFans();
-
-        displayFans(useFans.inst1);
-        displayFans(useFans.inst2);
-        displayFans(useFans.inst3);
-        displayFans(useFans.inst4);
-    }
-
-    public static void main2(String [] args){
+    public static void main2(String [] args) {
         UseFans useFans = new UseFans();
 
         displayFans(useFans.inst1);
@@ -29,6 +20,33 @@ class UseFans {
 }
 
 public class ModuleTwo{
-    public static void main(String [] args){
+    public static void main(String [] args) {
+        System.out.println("Method Displaying Multiple Instances:");
+        many(args);
+        System.out.println("\nMethod Displaying a Single Instance");
+        one(args);
+        System.out.println("");
+        System.out.println("Test Code");
+        create(args);
+    }
+    public static void many(String [] args) {
+        UseFans useFans = new UseFans();
+
+        UseFans.displayFans(useFans.inst1);
+        UseFans.displayFans(useFans.inst2);
+        UseFans.displayFans(useFans.inst3);
+        UseFans.displayFans(useFans.inst4);
+    }
+    public static void one(String [] args) {
+        UseFans oneFan = new UseFans();
+
+        UseFans.displayFans(oneFan.inst2);
+    }
+    public static void create(String [] args) {
+        Fan test1 = new Fan(Fan.SLOW, true, 3, "brown");
+        Fan test2 = new Fan(Fan.FAST, true, 1, "pink");
+
+        UseFans.displayFans(test1);
+        UseFans.displayFans(test2);
     }
 }

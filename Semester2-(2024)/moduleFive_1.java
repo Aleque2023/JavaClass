be7b25a7abcd9c435c4fe819d3ac6db2fe9ebad0
Skysight2023 <124235894+Skysight2023@users.noreply.github.com>
2024-02-04 11:01:23 -0600

    In progress

--- Semester2-(2024)/moduleFive_1.java
+++ Semester2-(2024)/moduleFive_1.java
@@ -1,8 +1,25 @@
 //Alejandro Quezada
-//2/3/2024
+//2/4/2024
 //Module 5 Programming Assignment
 
+import java.util.ArrayList;
 
 public class moduleFive_1 {
-    
+    public static void main(String[] args) {
+        ArrayList<String> myList = new ArrayList<>();
+        myList.add("The Hobbit");
+        myList.add("Castlevania");
+        myList.add("Tom and Jerry");
+        myList.add("Suits");
+        myList.add("Mr. Robot");
+        myList.add("Dune");
+        myList.add("Game of Thrones");
+        myList.add("Regular Show");
+        myList.add("Adventure Time");
+        myList.add("Rick and Morty");
+        
+        for (String string : myList) {
+            System.out.println(string);
+        }
+    }
 }

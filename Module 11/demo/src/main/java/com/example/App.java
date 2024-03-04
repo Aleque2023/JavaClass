07707811c4466011696fc264ad1a82f7478a6c96
Skysight2023 <124235894+Skysight2023@users.noreply.github.com>
2024-03-03 18:19:13 -0600

    Finished

--- Module 11/demo/src/main/java/com/example/App.java
+++ Module 11/demo/src/main/java/com/example/App.java
@@ -1,14 +1,18 @@
 package com.example;
 import javafx.application.*;
+import javafx.beans.Observable;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-
+import javafx.scene.paint.*;
 import java.util.*;
 import java.net.MalformedURLException;
 import javafx.event.EventHandler;
@@ -21,42 +25,88 @@ import javafx.scene.shape.Rectangle;
 import java.io.*;
 
 /**
- * JavaFX App
+ * JavaFX App 3/3/24 AQ Module 11
  */
 public class App extends Application {
-
-    private static Scene scene;
+    ToggleGroup grp = new ToggleGroup();
+    RadioButton cBtn = new RadioButton("Circle");
+    RadioButton rBtn = new RadioButton("Rectangle");
+    RadioButton eBtn = new RadioButton("Ellipse");
+    StackPane shape = new StackPane();
+    Label label = new Label("Pick a shape");
+    CheckBox fillCheckBox = new CheckBox("Random Color");
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-
+        shape.setPrefSize(300, 300);
         primaryStage.setTitle("RadioBtns, AQ");
-        ToggleGroup grp = new ToggleGroup();
-        RadioButton cBtn = new RadioButton("Circle");
-        RadioButton rBtn = new RadioButton("Rectangle");
-        RadioButton eBtn = new RadioButton("Ellipse");
-        TilePane tilePane = new TilePane();
+        HBox hBox = new HBox(10, cBtn, rBtn, eBtn, fillCheckBox);
+        hBox.setAlignment(Pos.BOTTOM_CENTER);
+
         cBtn.setToggleGroup(grp);
         rBtn.setToggleGroup(grp);
         eBtn.setToggleGroup(grp);
 
-        if (cBtn.isSelected()) {
-            Circle circle = new Circle();
-        }
-        else if (rBtn.isSelected()) {
-            Rectangle rectangle = new Rectangle();
-        }
-        else if (eBtn.isSelected()) {
-            Ellipse ellipse = new Ellipse();
-        }
+        StackPane sp = new StackPane(shape, hBox);
 
-        tilePane.getChildren().addAll(cBtn, rBtn, eBtn);
-        tilePane.setAlignment(Pos.BOTTOM_CENTER);
+        cBtn.setOnAction(event -> Circle());
+        rBtn.setOnAction(event -> Rectangle());
+        eBtn.setOnAction(event -> Ellipse());
+        fillCheckBox.setOnAction(event -> ckBox());
 
-        HBox hb1 = new HBox();
-        Scene scene = new Scene(tilePane, 200, 200);
+        Scene scene = new Scene(sp, 200, 200);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public void Circle() {
+        shape.getChildren().clear();
+        Circle circle = new Circle();
+        circle.setCenterX(409.0f);
+        circle.setCenterY(200.0f);
+        circle.setRadius(100.0f);
+        circle.setStroke(Color.BLACK);
+        if (fillCheckBox.isSelected()) {
+            circle.setFill(random());
+        }
+        shape.getChildren().add(circle);
+    }
+
+    public void Rectangle() {
+        shape.getChildren().clear();
+        Rectangle rectangle = new Rectangle();
+        rectangle.setWidth(222);
+        rectangle.setHeight(100);
+        rectangle.setStroke(Color.BLACK);
+        if (fillCheckBox.isSelected()) {
+            rectangle.setFill(random());
+        }
+        shape.getChildren().add(rectangle);
+    }
+
+    public void Ellipse() {
+        shape.getChildren().clear();
+        Ellipse ellipse = new Ellipse(300, 200);
+        ellipse.setStroke(Color.BLACK);
+        if (fillCheckBox.isSelected()) {
+            ellipse.setFill(random());
+        }
+        shape.getChildren().add(ellipse);
+    }
+
+    public void ckBox() {
+        if (cBtn.isSelected()) {
+            Circle();
+        } else if (rBtn.isSelected()) {
+            Rectangle();
+        } else if (eBtn.isSelected()) {
+            Ellipse();
+        }
+    }
+
+    public Color random() {
+        Random random = new Random();
+        return Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256));
     }
 
     public static void main(String[] args) {

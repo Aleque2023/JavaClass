9563abbfdbba646239ffa67eb097a54f2fbef1be
Skysight2023 <124235894+Skysight2023@users.noreply.github.com>
2024-03-24 19:00:02 -0500

    wip

--- AwriteMod_1.java
+++ AwriteMod_1.java
@@ -1,10 +1,10 @@
-package src;
+
 import java.io.*;
 
 public class AwriteMod_1 {
     public static void main(String[] args) {
         try {
-            FileWriter mWriter = new FileWriter("C:\\Users\\Alex\\Documents\\GitHub\\JavaClass\\src\\Alejandrodatafile.dat");
+            FileWriter mWriter = new FileWriter("C:\\Users\\Alex\\Documents\\GitHub\\JavaClass\\Alejandrodatafile.dat");
             mWriter.write("This is a test.");
             mWriter.close();
             System.out.println("Program has successfully written to designated file.");
